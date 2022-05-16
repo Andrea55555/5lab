3bb57510b16f379b458d9d49ab0bbe9d0f830691
Andrea55555 <47789560+Andrea55555@users.noreply.github.com>
2022-05-16 20:32:30 +0300

    finished12lab

--- conftest.py
+++ conftest.py
@@ -1,12 +1,31 @@
 import datetime
-import os
-
 import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import logging
+import os
 
-from myutils.utils import setup_logging
+path = os.getcwd()
+if not os.path.isdir("logs"):
+    os.mkdir("logs")
+
+
+def setup_logging(log_level, test_name):
+    check_and_create_logging_dir()
+    logger = logging.getLogger(__name__)
+    logger_handler = logging.FileHandler(f"allure-results/{test_name}.log", mode="a")
+    logger.setLevel(level=log_level)
+    logger_formatter = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y/%m/%d %H:%M:%S")
+    logger_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_handler)
+    return logger
+
+
+def check_and_create_logging_dir():
+    allure_logging_dir = os.path.join("C:\\", "d", "5lab", "../tests", "allure-results")
+    if not os.path.exists(allure_logging_dir):
+        os.mkdir(allure_logging_dir)
 
 DRIVERS_DIRECTORY = os.path.expanduser("~/Dev/drivers")
 
